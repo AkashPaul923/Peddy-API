4612c9d7ea884c183d46e1ed0e9bd27eee98c3eb
AkashPaul923 <akashpaul923@gmail.com>
2024-10-06 22:43:46 +0600

    show modal when clicked detail btn

--- js/index.js
+++ js/index.js
@@ -12,10 +12,25 @@ const loadpets = async () =>{
     displaypets(data.pets)
 }
 // load catagory pets
-const loadCatagorypets = async (category) =>{
+const loadCatagorypets = async (category , id) =>{
     const res = await fetch(`https://openapi.programming-hero.com/api/peddy/category/${category}`)
     const data = await res.json()
     displaypets(data.data)
+    activeBtn(id)
+    
+}
+// active btn
+const activeBtn = (id) => {
+    document.getElementById("btn-1").classList.remove("bg-[#00bba6]")
+    document.getElementById("btn-1").classList.remove("text-black")
+    document.getElementById("btn-2").classList.remove("bg-[#00bba6]")
+    document.getElementById("btn-2").classList.remove("text-black")
+    document.getElementById("btn-3").classList.remove("bg-[#00bba6]")
+    document.getElementById("btn-3").classList.remove("text-black")
+    document.getElementById("btn-4").classList.remove("bg-[#00bba6]")
+    document.getElementById("btn-4").classList.remove("text-black")
+    document.getElementById(`btn-${id}`).classList.add("bg-[#00bba6]")
+    document.getElementById(`btn-${id}`).classList.add("text-black")
 }
 // {
 //     "petId": 1,
@@ -33,14 +48,14 @@ const loadCatagorypets = async (category) =>{
 
 
 // display all pets
-const displaypets = (data) =>{
+function displaypets(data) {
     // console.log(data)
     const petCardContainer = document.getElementById('pet-card-container')
-    petCardContainer.innerHTML=''
-    if(data.length === 0){
+    petCardContainer.innerHTML = ''
+    if (data.length === 0) {
         // console.log('NO data found')
-        petCardContainer.innerHTML=`
-        <div class= "p-5 rounded-xl border border-solid lg:col-span-3 bg-gray-200 py-40 w-full">
+        petCardContainer.innerHTML = `
+        <div class= "p-5 rounded-xl border border-solid md:col-span-2 lg:col-span-3 bg-gray-200 py-40 w-full">
             <div class= "flex flex-col justify-center items-center gap-7">
                 <img class="mx-auto" src="assets/error.webp" alt="">
                 <p class="text-3xl font-extrabold text-center">No Data Found</p>
@@ -49,10 +64,10 @@ const displaypets = (data) =>{
         `
         return
     }
-    data.forEach(item =>{
+    data.forEach(item => {
         // console.log(item)
         const div = document.createElement('div')
-        div.innerHTML= `
+        div.innerHTML = `
         <div class=" p-5 rounded-xl border border-solid ">
             <div class=" h-[250px]" >
                 <img class="rounded-xl w-full h-full object-cover" src="${item.image}" alt="">
@@ -67,7 +82,7 @@ const displaypets = (data) =>{
             <div class="flex justify-between">
                 <button onclick="displayImage('${item.image}')" class="btn btn-outline btn-accent  text-lg font-bold"><i class="fa-regular fa-thumbs-up"></i></button>
                 <button class="btn btn-outline btn-accent text-lg font-bold">Adopt</button>
-                <button class="btn btn-outline btn-accent text-lg font-bold">Detail</button>
+                <button onclick="loadDetail('${item.petId}')" class="btn btn-outline btn-accent text-lg font-bold">Detail</button>
             </div>
         </div>
         `
@@ -87,9 +102,46 @@ const displayImage = (image) =>{
     `
     imageContainer.appendChild(div)
  }
+// show detail
+const loadDetail = async(petId) =>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/peddy/pet/${petId}`)
+    const data = await res.json()
+    showDetail(data.petData)
+    
+}
+const showDetail = (data) =>{
+    console.log(data)
+    const modalContant = document.getElementById('modal-contant')
+    modalContant.innerHTML=`
+    <div class=" h-[250px]" >
+        <img class="rounded-xl w-full h-full object-cover" src="${data.image}" alt="">
+    </div>
+    <div class="my-6 text-base text-gray-500 space-y-2">
+        <h1 class="font-bold text-xl text-black">${data.pet_name}</h1>
+        <p><i class="fa-solid fa-border-all"></i> <span> Breed : ${data.breed}</span></p>
+        <p><i class="fa-regular fa-calendar-days"></i><span> Birth : ${data.date_of_birth}</span></p>
+        <p><i class="fa-solid fa-mercury"></i> <span> Gender : ${data.gender}</span></p>
+        <p><i class="fa-solid fa-dollar-sign"></i> <span> Price : ${data.price}$</span></p>
+        <p><i class="fa-solid fa-virus"></i> <span> Vaccinated Status : ${data.vaccinated_status}</span></p>
+        <p class="font-bold text-xl text-black">Detail Information</p>
+        <p>${data.pet_details}</p>
+    </div>
+    `
+    document.getElementById('modal-btn').click()
+}
 
-
-
+// {
+//     "petId": 9,
+//     "breed": "Beagle",
+//     "category": "Dog",
+//     "date_of_birth": "2023-03-22",
+//     "price": null,
+//     "image": "https://i.ibb.co.com/XyXBtb8/pet-9.jpg",
+//     "gender": "Male",
+//     "pet_details": "This male Beagle, born on March 22, 2023, is curious, playful, and great with children. Fully vaccinated and priced at $1900, he is perfect for families looking for an active, adventurous companion that loves to explore.",
+//     "vaccinated_status": "Fully",
+//     "pet_name": "Buddy"
+// }
 
 
 
@@ -105,10 +157,10 @@ const displayCatagories = (data) =>{
    data.forEach((item) => {
     const div = document.createElement('div')
    div.innerHTML = `
-   <button onclick="loadCatagorypets('${item.category}')" class="btn btn-outline btn-accent md:btn-lg flex items-center justify-center">
+   <button id="btn-${item.id}" onclick="loadCatagorypets('${item.category}','${item.id}')" class="btn btn-outline btn-accent md:btn-lg flex items-center justify-center">
         <div class="flex items-center gap-1 md:gap-3 lg:gap-5">
             <img class="w-[15px] md:w-[25px] lg:w-[40px]" src="${item.category_icon}" alt="">
-            <p class="text-xs md:text-lg lg:text-2xl font-bold">${item.category}</p>
+            <p class="text-xs md:text-lg lg:text-2xl font-bold text-black">${item.category}</p>
         </div>
     </button>
    `
